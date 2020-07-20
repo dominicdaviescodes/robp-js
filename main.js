314ee44ca0af48a4634e736df5ffacb1c351fbfb
dominicdaviescodes <codewithdom@gmail.com>
2020-07-20 22:49:33 +0100

    L.96 Manipulating Styles With Javascript

--- main.js
+++ main.js
@@ -27,3 +27,25 @@ document.getElementById('secondButton').onclick = function () {
 document.getElementById('createParagraph').onclick = function () {
   document.getElementById('emptyParagraph').innerHTML = "some text";
 }
+
+// Manipulating Styles With Javascript
+
+document.getElementById('styleTextRed').onclick = function () {
+  // add style to the paragraph - red
+  document.getElementById('moreText').style.color = "red";
+}
+
+document.getElementById('styleTextBig').onclick = function () {
+  // add style to the paragraph - big
+  document.getElementById('moreText').style.fontSize = "20px";
+}
+
+document.getElementById('disappear').onclick = function () {
+  // add style to the paragraph - disappear
+  document.getElementById('moreText').style.display = "none";
+}
+
+document.getElementById('reappear').onclick = function () {
+  // add style to the paragraph - reappear
+  document.getElementById('moreText').style.display = "block";
+}
