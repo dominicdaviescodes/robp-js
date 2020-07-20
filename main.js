894cc3c18a12e11ada5e4960884c49a342e5fa35
dominicdaviescodes <codewithdom@gmail.com>
2020-07-20 21:27:51 +0100

    L.92 respond to a click

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 // access element and change it's content.
 document.getElementById('text').innerHTML = 'Hello Rob!';
+
+// access the button
+document.getElementById('myButton').onclick = function () {
+  document.getElementById('text').innerHTML = 'Text Changed';
+}
