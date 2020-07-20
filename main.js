3e314306908cca00e9cba7723fd2b2cbc61df870
dominicdaviescodes <codewithdom@gmail.com>
2020-07-20 22:37:19 +0100

    L.94 make text appear from nowhere

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ document.getElementById('text').innerHTML = 'Hello Rob!';
 document.getElementById('myButton').onclick = function () {
   document.getElementById('text').innerHTML = 'Text Changed';
 }
+
+// appending
+
+document.getElementById('secondButton').onclick = function () {
+  document.getElementById('secondParagraph').innerHTML =
+    document.getElementById('secondParagraph').innerHTML + " awesome";
+}
+
+// prepend
+
+document.getElementById('secondButton').onclick = function () {
+  document.getElementById('secondParagraph').innerHTML =
+    'I think ' +
+    document.getElementById('secondParagraph').innerHTML +
+    " awesome";
+}
+
+// make text appear from nowhere
+
+document.getElementById('createParagraph').onclick = function () {
+  document.getElementById('emptyParagraph').innerHTML = "some text";
+}
