5fcb0d1234d8bb679401fa7be7b34b7df810701b
dominicdaviescodes <codewithdom@gmail.com>
2020-07-21 18:02:30 +0100

    L98 Mini Challenge Disappearing Circles

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ document.getElementById('text').innerHTML = 'Hello Rob!';
 // access the button
 document.getElementById('myButton').onclick = function () {
   document.getElementById('text').innerHTML = 'Text Changed';
-}
+};
 
 // appending
 
 document.getElementById('secondButton').onclick = function () {
   document.getElementById('secondParagraph').innerHTML =
-    document.getElementById('secondParagraph').innerHTML + " awesome";
-}
+    document.getElementById('secondParagraph').innerHTML + ' awesome';
+};
 
 // prepend
 
@@ -19,33 +19,47 @@ document.getElementById('secondButton').onclick = function () {
   document.getElementById('secondParagraph').innerHTML =
     'I think ' +
     document.getElementById('secondParagraph').innerHTML +
-    " awesome";
-}
+    ' awesome';
+};
 
 // make text appear from nowhere
 
 document.getElementById('createParagraph').onclick = function () {
-  document.getElementById('emptyParagraph').innerHTML = "some text";
-}
+  document.getElementById('emptyParagraph').innerHTML = 'some text';
+};
 
 // Manipulating Styles With Javascript
 
 document.getElementById('styleTextRed').onclick = function () {
   // add style to the paragraph - red
-  document.getElementById('moreText').style.color = "red";
-}
+  document.getElementById('moreText').style.color = 'red';
+};
 
 document.getElementById('styleTextBig').onclick = function () {
   // add style to the paragraph - big
-  document.getElementById('moreText').style.fontSize = "20px";
-}
+  document.getElementById('moreText').style.fontSize = '20px';
+};
 
 document.getElementById('disappear').onclick = function () {
   // add style to the paragraph - disappear
-  document.getElementById('moreText').style.display = "none";
-}
+  document.getElementById('moreText').style.display = 'none';
+};
 
 document.getElementById('reappear').onclick = function () {
   // add style to the paragraph - reappear
-  document.getElementById('moreText').style.display = "block";
-}
+  document.getElementById('moreText').style.display = 'block';
+};
+
+// Mini challenge -
+
+document.getElementById('red').onclick = function () {
+  document.getElementById('red').style.display = 'none';
+};
+
+document.getElementById('yellow').onclick = function () {
+  document.getElementById('yellow').style.display = 'none';
+};
+
+document.getElementById('blue').onclick = function () {
+  document.getElementById('blue').style.display = 'none';
+};
