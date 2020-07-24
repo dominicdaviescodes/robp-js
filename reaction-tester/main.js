8ff13337eebd0682c438605d47f6e3508239e498
dominicdaviescodes <codewithdom@gmail.com>
2020-07-24 18:49:04 +0100

    make shape reappear after it has been clicked.

--- reaction-tester/main.js
+++ reaction-tester/main.js
@@ -6,11 +6,15 @@ function makeShapeAppear() {
   start = new Date().getTime();
 }
 
-setTimeout(makeShapeAppear, Math.random() * 2000);
+function appearAfterDelay() {
+  setTimeout(makeShapeAppear, Math.random() * 2000);
+}
+appearAfterDelay();
 
 document.getElementById('shape').onclick = function () {
   document.getElementById('shape').style.display = "none";
   var end = new Date().getTime();
   var timeTaken = (end - start) / 1000;
   document.getElementById('timeTaken').innerHTML = timeTaken + 's';
+  appearAfterDelay();
 }
