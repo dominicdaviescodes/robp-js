0ec6217a07e86be43d7df10b8f59062b6377eee8
dominicdaviescodes <codewithdom@gmail.com>
2020-07-24 18:20:43 +0100

    create shape, make it disappear when clicked, measure time taken to click on image

---
+++ reaction-tester/main.js
@@ -0,0 +1,7 @@
+var start = new Date().getTime();
+document.getElementById('shape').onclick = function () {
+  document.getElementById('shape').style.display = "none";
+  var end = new Date().getTime();
+  var timeTaken = (end - start) / 1000;
+  document.getElementById('timeTaken').innerHTML = timeTaken + 's';
+}
